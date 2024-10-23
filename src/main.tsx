134a6b9e8cb846bb3b54b873829771f47fca37f7
Shramee Srivastav <shramee.srivastav@gmail.com>
2024-10-23 17:12:18 +0700

    chore: add starknet provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StarknetProvider } from './components/starknet-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StarknetProvider>
+      <App />
+    </StarknetProvider>
   </StrictMode>,
 )
